9d6c6702fcbedb97f91ec1c32d6f941ad4408669
raph <r.berthier95@outlook.com>
2022-02-07 10:46:10 +0100

    front

---
+++ frontend/groupomania-app/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'signin',
+    component: () => import('../views/signin.vue')
+  },
+  {
+    path: '/perso',
+    name: 'perso',
+    component: () => import('../views/perso.vue'),
+  },
+  {  
+    path: '/wall',
+    name: 'wall',
+    component: () => import('../views/wall.vue')
+  },
+  {
+    path: '/profil',
+    name: 'profil',
+    component: () => import('../views/profil.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const Access = ["/"]
+  const pagesVerif = !Access.includes(to.path)
+  const loggedIn = sessionStorage.getItem("userId")
+  const sessionToken = sessionStorage.getItem("token")
+  if (pagesVerif && !loggedIn && !sessionToken) {
+      return next("/")
+  }
+  next()
+})
+
+export default router
